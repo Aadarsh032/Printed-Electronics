72d8f9b987fed7768e74996885e89679670e9be7
Aadarsh Kumar <123838450+Aadarsh032@users.noreply.github.com>
2024-10-02 19:15:48 +0530

    Update homedisplay.js

--- Printed-Electronics/src/assets/homedisplay.js
+++ Printed-Electronics/src/assets/homedisplay.js
@@ -3,6 +3,7 @@ import secondimg from './2.jpeg'
 import thirdimg from './3.jpeg'
 import fourthimg from './4.jpeg'
 import fifthimg from './5.jpeg'
+import sixthimg from './6.jpeg'
 
 
 
@@ -25,6 +26,10 @@ const homedisplay = [
     },
     {
         image: fifthimg,
+    
+    },
+    {
+        image: sixthimg,
     
     }
 
