1face4d522a7d7916030fed26a7fe423ceb525c9
Aadarsh Kumar <123838450+Aadarsh032@users.noreply.github.com>
2024-10-02 19:43:10 +0530

    Update research_projects.js

--- Printed-Electronics/src/assets/research_projects.js
+++ Printed-Electronics/src/assets/research_projects.js
@@ -11,6 +11,13 @@ const research_projects =[
         sanctioned_amount:"₹30 lakhs",
         funding_agency:"Science and Engineering Research Board"
     
+    },
+    {
+        topic_name:"A general route towards low voltage, high-current power printed electronics.",
+        project_instructor:`Suresh Kumar Garlapati`,
+        sanctioned_amount:"₹30 lakhs",
+        funding_agency:"Science and Engineering Research Board"
+    
     }
 ]
 export default research_projects;
