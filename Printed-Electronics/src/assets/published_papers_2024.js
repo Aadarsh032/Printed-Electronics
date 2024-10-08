9f020d16f5f52f9e76c95824b29535afc655ce9d
Aadarsh032 <kumarn206@gmail.com>
2024-10-08 13:22:35 +0530

    Added a Research Paper

--- Printed-Electronics/src/assets/published_papers_2024.js
+++ Printed-Electronics/src/assets/published_papers_2024.js
@@ -1,5 +1,15 @@
 const published_papers_2024 =[
     {
+        topic_name:"Development and Characterization of Biocompatible Cellulose Acetate Substrate for Flexible Electrochemical Biosensors",
+        topic_authors:`Karri Trinadha Rao, Rahul Gangwar, Aditya Bhagavathi, Sajmina Khatun, Pravat Kumar Sahu, Aravind Kumar Rengan, Challapalli Subrahmanyam, Suresh Kumar Garlapati, Siva Rama Krishna Vanjari`,
+        topic_link:"https://ieeexplore.ieee.org/abstract/document/10614637/authors#authors"
+    },
+    {
+        topic_name:"Compliance-free, analog RRAM devices based on SnOx",
+        topic_authors:`Suresh Kumar Garlapati, Firman Mangasa Simanjuntak,  Spyros Stathopoulos,  A Syed Jalaluddeen, Mari Napari, Themis Prodromakis`,
+        topic_link:"https://www.nature.com/articles/s41598-024-64662-9"
+    },
+    {
         topic_name:"A Flexible Smart Sensor System based on Hybrid Integration of Organic and Metal Oxide Transistors",
         topic_authors:`Moon Hyo Kang, Iyad Nasrallah, Sheida Faraji, Suresh K Garlapati, Aiman Rahmanudin, Daniel J Tate, Glenn Sunley Saez, Krishna C Persaud, Michael L Turner, Henning Sirringhaus`,
         topic_link:"https://scholar.google.com/citations?view_op=view_citation&hl=en&user=Or_ygS0AAAAJ&pagesize=80&citation_for_view=Or_ygS0AAAAJ:9ZlFYXVOiuMC"
